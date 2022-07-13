3bafde77c7ec93fd5ca971699f37465b239bf4df
unknown <ryancare@protonmail.com>
2022-07-13 19:44:24 -0400

    Added generator for fake dummy data, defaulting to 10 entries returned

--- api/data.js
+++ api/data.js
@@ -1,4 +1,17 @@
-module.exports = [
-    {topic: "topic one"},
-    {topic: "topics two"}
-]
+const asylumOffice = () => ["zar", "zbo", "zch", "zhn", "zla", "zmi", "znk", "zny", "zol", "zsf"][Math.floor(Math.random() * 10)];
+const citizenship = () => ["Afghanistan", "Albania", "Algeria", "Angola"][Math.floor(Math.random() * 4)];
+const raceOrEthnicity = () => ["Armenian", "Chinese", "Albanian"][Math.floor(Math.random() * 3)];
+const caseOutcome = () => ["Deny/Referral", "Grant", "Admin Close/Dismissal"][Math.floor(Math.random() * 3)];
+const completion = () => new Date(Date.now() - Math.random() * 36 * 1000 * 60 * 60 * 24 * 30).toISOString();
+const currentDate = () => new Date(Date.now()).toISOString();
+
+module.exports = Array.from({length: 10}, () => {
+  return {
+    "asylumOffice": asylumOffice(),
+    "citizenship" : citizenship(), 
+    "raceOrEthnicity" : raceOrEthnicity(),
+    "caseOutcome" : caseOutcome(),
+    "completion" : completion(),
+    "currentDate" : currentDate()
+  }
+})
