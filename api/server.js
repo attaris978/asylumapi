dcbdbec570a47d94beb920c290fd38b203de2f08
unknown <ryancare@protonmail.com>
2022-07-13 20:20:27 -0400

    Added CORS middleware to allow fetch capability

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const generateData = require('./data');
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
