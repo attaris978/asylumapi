9934838bd65acc957d410a9a8197ca923b7da714
unknown <ryancare@protonmail.com>
2022-07-14 06:59:34 -0400

    Changed endpoint to reflect ultimate api layout

--- api/server.js
+++ api/server.js
@@ -7,12 +7,12 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/', (req, res) => {
+server.get('/cases', (req, res) => {
     let data = generateData();    
     res.status(201).json(data)
 })
 
-server.get('/:numberOfEntries', (req, res) => {
+server.get('/cases/:numberOfEntries', (req, res) => {
     let numberOfEntries = +req.params.numberOfEntries;
     let data = generateData(numberOfEntries);    
     res.status(201).json(data)
