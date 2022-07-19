adbeb293d57bc2f393a47cd4bcaaf099f859a273
unknown <ryancare@protonmail.com>
2022-07-19 10:22:50 -0400

    Updated default for generated cases to 10000 to allow for seamless integration with frontend filtering functionality

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ server.use(cors());
 server.use(express.json());
 
 server.get('/cases', (req, res) => {        
-    let data = req.query?.count ? generateData(req.query.count) : generateData();    
+    let data = req.query?.count ? generateData(req.query.count) : generateData(10000);    
     let filteredData = filterData(req,data);
     res.status(201).json(filteredData)
 })
