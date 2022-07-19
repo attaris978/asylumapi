d68d894c23a628bd250c292ae9172542eb17cb46
unknown <ryancare@protonmail.com>
2022-07-19 10:15:18 -0400

    Added all possible citizenships and regional office configurations and modified generateData function to include them

--- api/data.js
+++ api/data.js
@@ -1,20 +1,31 @@
-const asylumOffice = () => ["zar", "zbo", "zch", "zhn", "zla", "zmi", "znk", "zny", "zol", "zsf"][Math.floor(Math.random() * 10)];
-const citizenship = () => ["Afghanistan", "Albania", "Algeria", "Angola"][Math.floor(Math.random() * 4)];
-const raceOrEthnicity = () => ["Armenian", "Chinese", "Albanian"][Math.floor(Math.random() * 3)];
-const caseOutcome = () => ["Deny/Referral", "Grant", "Admin Close/Dismissal"][Math.floor(Math.random() * 3)];
-const completion = () => new Date(Date.now() - Math.random() * 36 * 1000 * 60 * 60 * 24 * 30).toISOString();
+const { regions, officeRegions } = require('./filterConstants');
+
+const officeList = officeRegions.map(office => office.code);
+const regionList = regions.map(region => region.territory);
+
+const asylumOffice = () =>
+  officeList[Math.floor(Math.random() * officeList.length)];
+const citizenship = () =>
+  regionList[Math.floor(Math.random() * regionList.length)];
+const caseOutcome = () =>
+  ['Deny/Referral', 'Grant', 'Admin Close/Dismissal'][
+    Math.floor(Math.random() * 3)
+  ];
+const completion = () =>
+  new Date(
+    Date.now() - Math.random() * 36 * 1000 * 60 * 60 * 24 * 30
+  ).toISOString();
 const currentDate = () => new Date(Date.now()).toISOString();
 
-const generateData = (numberOfEntries = 20) => Array.from({length: numberOfEntries}, (v,i) => {
-  return {
-    "caseId" : i,
-    "asylumOffice": asylumOffice(),
-    "citizenship" : citizenship(), 
-    "raceOrEthnicity" : raceOrEthnicity(),
-    "caseOutcome" : caseOutcome(),
-    "completion" : completion(),
-    "currentDate" : currentDate()
-  }
-})
+const generateData = (numberOfEntries = 200) =>
+  Array.from({ length: numberOfEntries }, () => {
+    return {
+      asylumOffice: asylumOffice(),
+      citizenship: citizenship(),
+      caseOutcome: caseOutcome(),
+      completion: completion(),
+      currentDate: currentDate(),
+    };
+  });
 
 module.exports = generateData;
